ef723743836863c310abc13ab85179bc3c1d51fc
louis <louis.lefebure@student.howest.be>
2023-11-18 12:48:06 +0100

    Initial commit (Angular PWA) + manifest file + Experimented with webcam and taking pictures

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {CameraComponent} from "./camera/camera.component";
+import {WebcamModule} from "ngx-webcam";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CameraComponent, WebcamModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
